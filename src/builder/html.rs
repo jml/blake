ac524612d34ab554a10e79289c91921b78aa9c55
Jonathan Lange <jml@mumak.net>
2019-10-18 17:26:06 +0100

    Parse title out

--- src/builder/html.rs
+++ src/builder/html.rs
@@ -1,9 +1,9 @@
-extern crate comrak;
-
 use std::fs;
 use std::path::Path;
 
 use chrono::prelude::*;
+use comrak;
+use comrak::nodes::{NodeHeading, NodeValue};
 use comrak::ComrakOptions;
 use lazy_static::lazy_static;
 use tera::{compile_templates, Tera};
@@ -32,15 +32,71 @@ pub fn build_html(source: &Path, dest: &Path, date: &DateTime<Utc>) {
     fs::write(dest, rendered).expect(&format!("Couldn't write to file: {}", dest.display()));
 }
 
-fn render_markdown(contents: &str) -> (&str, String) {
+fn render_markdown(contents: &str) -> (Option<String>, String) {
     let arena = comrak::Arena::new();
     let root = comrak::parse_document(&arena, contents, &ComrakOptions::default());
+    let title = find_title(root).map(|s| s.to_owned());
     let mut html = vec![];
     comrak::format_html(root, &ComrakOptions::default(), &mut html).expect("Couldn't format HTML");
     let html_str = String::from_utf8(html).expect("Invalid unicode");
-    ("", html_str)
+    (title, html_str)
     // TODO: Fork
     // file:///Users/jml/src/blake/target/doc/src/comrak/html.rs.html#13-28
     // and change footnotes to sidenotes.
-    // TODO: Make a new function to extra title from markdown AST
+}
+
+/// Find the title in the post.
+///
+/// Assumes that the first Heading 1 is the title.
+fn find_title<'a>(root: &'a comrak::nodes::AstNode<'a>) -> Option<String> {
+    for node in root.descendants() {
+        if let NodeValue::Heading(NodeHeading { level: 1, .. }) = node.data.borrow().value {
+            // TODO: Is there a way to avoid the clone?
+            let text = node
+                .children()
+                .filter_map(|child| child.data.borrow().value.text().cloned())
+                .flatten()
+                .collect::<Vec<u8>>();
+            return String::from_utf8(text).ok();
+        }
+    }
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_no_title() {
+        let arena = comrak::Arena::new();
+        let contents = "no heading here";
+        let root = comrak::parse_document(&arena, contents, &ComrakOptions::default());
+        assert_eq!(find_title(&root), None)
+    }
+
+    #[test]
+    fn test_title() {
+        let arena = comrak::Arena::new();
+        let contents = "# title
+
+paragraph text";
+        let root = comrak::parse_document(&arena, contents, &ComrakOptions::default());
+        assert_eq!(find_title(&root), Some(String::from("title")))
+    }
+
+    #[test]
+    fn test_two_titles() {
+        let arena = comrak::Arena::new();
+        let contents = "# title
+
+paragraph text
+
+# second title
+
+more text
+";
+        let root = comrak::parse_document(&arena, contents, &ComrakOptions::default());
+        assert_eq!(find_title(&root), Some(String::from("title")))
+    }
 }
