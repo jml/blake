50562e73b8d9ea1499cb9eb1c07f8a536b2a3837
Jonathan Lange <jml@mumak.net>
2019-10-18 14:30:36 +0100

    Move code to library

---
+++ src/lib.rs
@@ -0,0 +1,106 @@
+use chrono::prelude::*;
+use std::ffi;
+use std::fs;
+use std::io;
+use std::path::Path;
+use std::process;
+
+mod builder;
+
+const POST_DATE_FORMAT: &str = "%Y-%m-%d-%H:%M";
+const POSTS_DIR: &str = "/Users/jml/src/notebook/posts/"; // Parameterize this.
+const STATIC_DIR: &str = "/Users/jml/src/notebook/static/"; // Parameterize this.
+const OUTPUT_DIR: &str = "/Users/jml/src/blake/output/"; // Parameterize this.
+
+
+/// Create a new blog post.
+pub fn new_post() {
+    let now = Utc::now();
+    let name = format!("{}", now.format(POST_DATE_FORMAT));
+    edit_and_commit_post(Path::new(POSTS_DIR), &name);
+}
+
+pub fn edit_post(name: Option<ffi::OsString>) {
+    let posts_dir = Path::new(POSTS_DIR);
+    let name = name.or_else(|| get_latest_file(posts_dir));
+    match name {
+        None => {
+            println!("Could not find post to edit.");
+        }
+        Some(n) => {
+            let name = Path::new(&n).file_stem().unwrap();
+            edit_and_commit_post(posts_dir, name.to_str().unwrap());
+        }
+    }
+}
+
+fn get_latest_file(posts_dir: &Path) -> Option<ffi::OsString> {
+    let entries = fs::read_dir(posts_dir)
+        .expect(&format!("Couldn't read directory: {}", posts_dir.display()));
+    entries
+        .filter_map(|entry| entry.ok().map(|e| e.file_name()))
+        .max()
+}
+
+pub fn build() {
+    let path = Path::new(OUTPUT_DIR);
+    let output = builder::OutputPath {
+        path: path.to_owned(),
+    };
+    builder::do_build(Path::new(STATIC_DIR), Path::new(POSTS_DIR), &output);
+}
+
+/// Edit the blog post with the given name inside the posts directory.
+///
+/// If it changes, ensure the change is committed.
+fn edit_and_commit_post(posts_dir: &Path, name: &str) {
+    let mut post_file = posts_dir.to_owned();
+    post_file.push(name);
+    post_file.set_extension("md");
+    let changed = edit_file(&post_file);
+    if changed {
+        process::Command::new("git")
+            .current_dir(posts_dir)
+            .arg("add")
+            .arg(post_file)
+            .status()
+            .expect("Could not add file");
+        process::Command::new("git")
+            .current_dir(posts_dir)
+            .arg("commit")
+            .arg("-m")
+            .arg(format!("Add new post {}", name))
+            .status()
+            .expect("Could not commit file");
+    }
+}
+
+fn edit_file(filename: &Path) -> bool {
+    let prev = contents(filename);
+    edit(filename);
+    let current = contents(filename);
+    prev != current
+}
+
+/// Get the contents of a file as a vector.
+///
+/// If the file doesn't exist, return None. Panic if we get any other kind of
+/// error.
+fn contents(path: &Path) -> Option<Vec<u8>> {
+    match fs::read(&path) {
+        Ok(bytes) => Some(bytes),
+        Err(err) => match err.kind() {
+            io::ErrorKind::NotFound => None,
+            _ => panic!("Could not read file: {}: {}", path.display(), err),
+        },
+    }
+}
+
+/// Edit a file in my preferred editor.
+fn edit(file: &Path) {
+    process::Command::new("emacsclient")
+        .arg("-c")
+        .arg(file)
+        .status()
+        .expect(&format!("Failed to edit file: {}", file.display()));
+}
